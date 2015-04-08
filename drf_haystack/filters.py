fd0ef6b69ae06d20af3b2218847a7f7844ef054b
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-04-08 15:45:47 +0200

    Fixed #2 ViewSet `get_object` should raise 404 if no object is found

--- drf_haystack/filters.py
+++ drf_haystack/filters.py
@@ -3,8 +3,8 @@
 from __future__ import absolute_import, unicode_literals
 
 import operator
-
 from itertools import chain
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import six
