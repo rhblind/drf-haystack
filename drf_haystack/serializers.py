ba39c24bb5b4535140dd85c19934e1dcca9608b6
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-06-14 00:09:36 +0200

    - moved serializer mixin to serializers.py - expclitly import fields

--- drf_haystack/serializers.py
+++ drf_haystack/serializers.py
@@ -6,12 +6,17 @@ import warnings
 from itertools import chain
 
 from django.core.exceptions import ImproperlyConfigured
+from django.utils import six
 
 from haystack import fields as haystack_fields
 from haystack.query import EmptySearchQuerySet
+from haystack.utils import Highlighter
 
 from rest_framework import serializers
-from rest_framework.fields import *
+from rest_framework.fields import (
+    BooleanField, CharField, DateField, DateTimeField,
+    DecimalField, FloatField, IntegerField
+)
 from rest_framework.utils.field_mapping import ClassLookupDict, get_field_kwargs
 
 
@@ -138,3 +143,29 @@ class HaystackSerializer(serializers.Serializer):
                 del ret[field]
         return ret
 
+
+class HighlighterMixin(object):
+    """
+    This mixin adds support for highlighting (the pure python
+    version, not SearchQuerySet().highlight()). See Haystack docs
+    for more info).
+    """
+
+    highlighter_class = Highlighter
+
+    def get_highlighter(self):
+        if not self.highlighter_class:
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a highlighter_class. Define %(cls)s.highlighter_class, "
+                "or override %(cls)s.get_highlighter()." %
+                {"cls": self.__class__.__name__}
+            )
+        return self.highlighter_class
+
+    def get_serializer_context(self):
+        context = super(HighlighterMixin, self).get_serializer_context()
+        words = " ".join(six.itervalues(self.request.GET))
+        highlighter = self.get_highlighter()(words)
+        if highlighter:
+            context["highlighter"] = highlighter
+        return context
