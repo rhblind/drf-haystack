28535e63d5de1caf66426fe4f263c119f8189fd4
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-08-12 13:01:22 +0200

    removed python-dateutil from requirements

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         "Django>=1.5.0",
         "djangorestframework>=2.4.4",
         "django-haystack>=2.3.1",
-        "python-dateutil>=2.0"
     ],
     tests_require=[
         "nose",
