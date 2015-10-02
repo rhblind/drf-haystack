a482ffd0a21acf4a1d3993b754888ff2bf6a1a5b
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-10-02 23:04:08 +0200

    added utils.py to hold the merge_dict function

---
+++ drf_haystack/utils.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+from copy import deepcopy
+from django.utils import six
+
+
+def merge_dict(a, b):
+    """
+    Recursively merges and returns dict a with dict b.
+
+    :param a: dictionary object
+    :param b: dictionary object
+    :return: merged dictionary object
+    """
+
+    if not isinstance(b, dict):
+        return b
+
+    result = deepcopy(a)
+    for key, val in six.iteritems(b):
+        if key in result and isinstance(result[key], dict):
+            result[key] = merge_dict(result[key], val)
+        else:
+            result[key] = deepcopy(val)
+
+    return result
