853a024bba1dd02840d1031b61c84ab3209bd9ea
sampeka <sam.i.peka@gmail.com>
2016-03-01 22:03:56 +0000

    Detached build_filter and build_facet_filter from Filter classes to QueryBuilder
    * Also begun to move settings to constants

--- drf_haystack/filters.py
+++ drf_haystack/filters.py
@@ -6,7 +6,6 @@ import operator
 import warnings
 from itertools import chain
 
-from dateutil import parser
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -17,7 +16,7 @@ from haystack.query import SearchQuerySet
 
 from rest_framework.filters import BaseFilterBackend
 
-from .utils import merge_dict
+from .query import QueryBuilder
 
 
 class HaystackFilter(BaseFilterBackend):
@@ -25,11 +24,14 @@ class HaystackFilter(BaseFilterBackend):
     A filter backend that compiles a haystack compatible filtering query.
     """
 
+    query_builder = QueryBuilder()
+
     @staticmethod
     def get_request_filters(request):
         return request.GET.copy()
 
-    def build_filters(self, view, filters=None):
+    @classmethod
+    def build_filter(cls, view, filters=None):
         """
         Creates a single SQ filter from querystring parameters that
         correspond to the SearchIndex fields that have been "registered"
@@ -41,61 +43,7 @@ class HaystackFilter(BaseFilterBackend):
         Any querystring parameters that are not registered in
         `view.fields` will be ignored.
         """
-
-        applicable_filters = []
-        applicable_exclusions = []
-
-        if filters is None:
-            filters = {}  # pragma: no cover
-
-        for param, value in filters.items():
-            # Skip if the parameter is not listed in the serializer's `fields`
-            # or if it's in the `exclude` list.
-            excluding_term = False
-            param_parts = param.split("__")
-            base_param = param_parts[0]  # only test against field without lookup
-            negation_keyword = getattr(settings, "DRF_HAYSTACK_NEGATION_KEYWORD", "not")
-            if len(param_parts) > 1 and param_parts[1] == negation_keyword:
-                excluding_term = True
-                param = param.replace("__%s" % negation_keyword, "")  # haystack wouldn't understand our negation
-
-            if view.serializer_class:
-                try:
-                    if hasattr(view.serializer_class.Meta, "field_aliases"):
-                        old_base = base_param
-                        base_param = view.serializer_class.Meta.field_aliases.get(base_param, base_param)
-                        param = param.replace(old_base, base_param)  # need to replace the alias
-
-                    fields = getattr(view.serializer_class.Meta, "fields", [])
-                    exclude = getattr(view.serializer_class.Meta, "exclude", [])
-                    search_fields = getattr(view.serializer_class.Meta, "search_fields", [])
-
-                    if ((fields or search_fields) and base_param not in chain(fields, search_fields)) or base_param in exclude or not value:
-                        continue
-
-                except AttributeError:
-                    raise ImproperlyConfigured("%s must implement a Meta class." %
-                                               view.serializer_class.__class__.__name__)
-
-            tokens = [token.strip() for token in value.split(view.lookup_sep)]
-            field_queries = []
-
-            for token in tokens:
-                if token:
-                    field_queries.append(view.query_object((param, token)))
-
-            term = six.moves.reduce(operator.or_, filter(lambda x: x, field_queries))
-            if excluding_term:
-                applicable_exclusions.append(term)
-            else:
-                applicable_filters.append(term)
-
-        applicable_filters = six.moves.reduce(
-            operator.and_, filter(lambda x: x, applicable_filters)) if applicable_filters else []
-        applicable_exclusions = six.moves.reduce(
-            operator.and_, filter(lambda x: x, applicable_exclusions)) if applicable_exclusions else []
-
-        return applicable_filters, applicable_exclusions
+        return cls.query_builder.build_query(view, **(filters if filters else {}))
 
     @staticmethod
     def apply_filters(queryset, applicable_filters=None, applicable_exclusions=None):
@@ -309,93 +257,12 @@ class HaystackFacetFilter(HaystackFilter):
 
     # TODO: Support multiple indexes/serializers
 
-    @staticmethod
-    def parse(lookup_sep, options):
-        """
-        Parse the field options query string and return it as a dictionary.
-        """
-        defaults = {}
-        if isinstance(options, six.text_type):
-
-            tokens = [token.strip() for token in options.split(lookup_sep)]
-
-            for token in tokens:
-                if not len(token.split(":")) == 2:
-                    warnings.warn("The %s token is not properly formatted. Tokens need to be "
-                                  "formatted as 'token:value' pairs." % token)
-                    continue
-
-                param, value = token.split(":")
-
-                if any([k == param for k in ("start_date", "end_date", "gap_amount")]):
-
-                    if param in ("start_date", "end_date"):
-                        value = parser.parse(value)
-
-                    if param == "gap_amount":
-                        value = int(value)
-
-                defaults[param] = value
-
-        return defaults
-
     def build_facet_filter(self, view, filters=None):
         """
         Creates a dict of dictionaries suitable for passing to the
         SearchQuerySet ``facet``, ``date_facet`` or ``query_facet`` method.
         """
-
-        field_facets = {}
-        date_facets = {}
-        query_facets = {}
-        facet_serializer_cls = view.get_facet_serializer_class()
-
-        if filters is None:
-            filters = {}  # pragma: no cover
-
-        if view.lookup_sep == ":":
-            raise AttributeError("The %(cls)s.lookup_sep attribute conflicts with the HaystackFacetFilter "
-                                 "query parameter parser. Please choose another `lookup_sep` attribute "
-                                 "for %(cls)s." % {"cls": view.__class__.__name__})
-
-        try:
-            fields = getattr(facet_serializer_cls.Meta, "fields", [])
-            exclude = getattr(facet_serializer_cls.Meta, "exclude", [])
-            field_options = getattr(facet_serializer_cls.Meta, "field_options", {})
-
-            for field, options in filters.items():
-
-                if field not in fields or field in exclude:
-                    continue
-
-                field_options = merge_dict(field_options, {field: self.parse(view.lookup_sep, options)})
-
-            valid_gap = ("year", "month", "day", "hour", "minute", "second")
-            for field, options in field_options.items():
-                if any([k in options for k in ("start_date", "end_date", "gap_by", "gap_amount")]):
-
-                    if not all(("start_date", "end_date", "gap_by" in options)):
-                        raise ValueError("Date faceting requires at least 'start_date', 'end_date' "
-                                         "and 'gap_by' to be set.")
-
-                    if not options["gap_by"] in valid_gap:
-                        raise ValueError("The 'gap_by' parameter must be one of %s." % ", ".join(valid_gap))
-
-                    options.setdefault("gap_amount", 1)
-                    date_facets[field] = field_options[field]
-
-                else:
-                    field_facets[field] = field_options[field]
-
-        except AttributeError:
-            raise ImproperlyConfigured("%s must implement a Meta class." %
-                                       facet_serializer_cls.__class__.__name__)
-
-        return {
-            "date_facets": date_facets,
-            "field_facets": field_facets,
-            "query_facets": query_facets
-        }
+        return self.query_builder.build_facet_query(view, **(filters if filters else {}))
 
     @staticmethod
     def apply_facets(queryset, filters):
