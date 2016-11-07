314882890a3f927d96cd9739c78b20473ce07e2b
Rolf Håvard Blindheim <rhblind@gmail.com>
2016-11-08 00:40:25 +0100

    Lock django-haystack to stable version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         "Django>=1.8,<1.11",
         "djangorestframework>=3.2.0",
-        "django-haystack>=2.5.dev1",
+        "django-haystack>=2.5.0",
         "python-dateutil"
     ],
     tests_require=[
