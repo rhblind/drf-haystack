ea928e260433689b34e7cea092f08672b8944f12
Johnny Wellington dos Santos <johnny.w.santos@gmail.com>
2017-04-07 23:09:47 -0300

    Add DRF_HAYSTACK_SPATIAL_QUERY_PARAM to constants
    It allow to change the default argument 'from' to anoter keywork,
    since it may cause some trouble when using Serializers to validate
    the query parameters, as this is a python reserved keyword.

--- drf_haystack/constants.py
+++ drf_haystack/constants.py
@@ -2,3 +2,4 @@ from django.conf import settings
 
 DRF_HAYSTACK_NEGATION_KEYWORD = getattr(settings, "DRF_HAYSTACK_NEGATION_KEYWORD", "not")
 GEO_SRID = getattr(settings, "GEO_SRID", 4326)
+DRF_HAYSTACK_SPATIAL_QUERY_PARAM = getattr(settings, "DRF_HAYSTACK_SPATIAL_QUERY_PARAM", "from")
