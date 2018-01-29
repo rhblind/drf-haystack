5104f9d4b9c343979e3a45994baebafa87970cd2
Rolf Håvard Blindheim <rhblind@gmail.com>
2018-01-30 00:42:55 +0100

    Bumped version
    Signed-off-by: Rolf Håvard Blindheim <rhblind@gmail.com>

--- drf_haystack/__init__.py
+++ drf_haystack/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 __title__ = "drf-haystack"
-__version__ = "1.7.1rc1"
+__version__ = "1.7.1rc2"
 __author__ = "Rolf Haavard Blindheim"
 __license__ = "MIT License"
 
