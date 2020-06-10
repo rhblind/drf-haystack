25528e65ac8155f3c978b1a84dd37126a615aeaf
Alexandre Dufour <adufour@poka.io>
2020-06-10 16:08:29 -0400

    Use basename instead of deprecated 'base_name'

--- tests/urls.py
+++ tests/urls.py
@@ -7,8 +7,8 @@ from rest_framework import routers
 from tests.mockapp.views import SearchPersonFacetViewSet, SearchPersonMLTViewSet
 
 router = routers.DefaultRouter()
-router.register("search-person-facet", viewset=SearchPersonFacetViewSet, base_name="search-person-facet")
-router.register("search-person-mlt", viewset=SearchPersonMLTViewSet, base_name="search-person-mlt")
+router.register("search-person-facet", viewset=SearchPersonFacetViewSet, basename="search-person-facet")
+router.register("search-person-mlt", viewset=SearchPersonMLTViewSet, basename="search-person-mlt")
 
 urlpatterns = [
     url(r"^", include(router.urls))
