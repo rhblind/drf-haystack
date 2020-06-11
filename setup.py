f9efe0df789a2e99a7609c73aec2105bc692f1f9
Alexandre Dufour <adufour@poka.io>
2020-06-11 11:43:32 -0400

    Change Django support to <3.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "Django>=2.2,<4.0",
+        "Django>=2.2,<3.1",
         "djangorestframework>=3.7,<3.12",
         "django-haystack>=2.8,<3.1",
         "python-dateutil"
