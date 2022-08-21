2ea23149d33cb8937d5b81d0ad2a3953be87712d
13KuntalRoy <roykuntal1213@gmail.com>
2022-08-22 01:46:02 +0530

    Django version  update

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "Django>=2.2,<3.3",
+        "Django>=2.2,<=4.1",
         "djangorestframework>=3.7,<3.13",
         "django-haystack>=2.8,<=3.2",
         "python-dateutil"
