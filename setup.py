c3985fd52832804c534f4b6d1c452cb98521ad8d
Hasan Javed <hasanjaved065@gmail.com>
2023-04-13 04:58:20 +0500

    Fix in dependecies.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "Django>=2.2,<=4.2",
-        "djangorestframework>=3.7,<3.14",
+        "djangorestframework>=3.7,<=3.14",
         "django-haystack>=2.8,<=3.2.1",
         "python-dateutil"
     ],
