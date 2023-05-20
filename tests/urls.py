bfaef28de1eca1d5cc39e57b7e7e0009749c601f
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2023-05-20 18:05:22 +0200

    fix: migrate urls.py to use path

--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import include, url
+from django.urls import path
+from django.conf.urls import include
 
 from rest_framework import routers
 
 from tests.mockapp.views import SearchPersonFacetViewSet, SearchPersonMLTViewSet
 
 router = routers.DefaultRouter()
-router.register("search-person-facet", viewset=SearchPersonFacetViewSet, basename="search-person-facet")
-router.register("search-person-mlt", viewset=SearchPersonMLTViewSet, basename="search-person-mlt")
+router.register(
+    "search-person-facet",
+    viewset=SearchPersonFacetViewSet,
+    basename="search-person-facet",
+)
+router.register(
+    "search-person-mlt", viewset=SearchPersonMLTViewSet, basename="search-person-mlt"
+)
 
-urlpatterns = [
-    url(r"^", include(router.urls))
-]
+urlpatterns = [path("", include(router.urls))]
