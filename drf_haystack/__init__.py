e0e748dda0ae7105f8a4a7b220653f6b6c6c11e8
Mikkel Munch Mortensen <3xm@detfalskested.dk>
2023-09-07 14:57:30 +0200

    Bump version for next release

--- drf_haystack/__init__.py
+++ drf_haystack/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 __title__ = "drf-haystack"
-__version__ = "1.8.12"
+__version__ = "1.8.13"
 __author__ = "Rolf Haavard Blindheim"
 __license__ = "MIT License"
 
