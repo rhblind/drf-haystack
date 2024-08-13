08460bdcf0493ea0ee9a279078570836a1c8e191
maasa142 <arai.masataka142@gmail.com>
2024-08-14 03:25:39 +0900

    Update install_requires

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "Django>=2.2,<4.3",
+        "Django>=2.2,<=5.0",
         "djangorestframework>=3.7,<3.16",
-        "django-haystack>=2.8,<=3.2.1",
+        "django-haystack>=2.8,<=3.3",
         "python-dateutil"
     ],
     tests_require=[
